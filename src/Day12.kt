dc13d8ccd20ac2315f08f376ca545b36f9891013
Marek Kohn <me@makohn.de>
2023-10-29 21:48:02 +0100

    Add solution for day 12 part 2

--- src/Day12.kt
+++ src/Day12.kt
@@ -2,10 +2,10 @@ fun main() {
 
     val day = "12"
 
-    fun part1(input: List<String>): Int {
+    fun solve(input: List<String>, predicate: (Int) -> Boolean, startWith: Char, vararg endWith: Char): Int {
         val inputMatrix = input.toCharMatrix()
         val dataPoints = inputMatrix.dataPoints()
-        val startNode = dataPoints.single { it.data == 'S' }
+        val startNode = dataPoints.single { it.data == startWith }
 
         fun Char.height() = when(this) {
             'S' -> 0
@@ -14,22 +14,26 @@ fun main() {
         }
 
         val visited = bfs(startNode) { p ->
-            inputMatrix.adjacentPoints(p).filter { it.data.height() - p.data.height() <= 1 }
+            inputMatrix.adjacentPoints(p).filter { predicate(it.data.height() - p.data.height()) }
         }
 
-        return visited.filter { it.key.data == 'E' }.minBy { it.value }.value
+        return visited.filter { it.key.data in endWith }.minBy { it.value }.value
+    }
+
+    fun part1(input: List<String>): Int {
+        return solve(input, { diff -> diff <= 1 }, 'S', 'E')
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return solve(input, { diff -> diff >= -1 }, 'E', 'a', 'S')
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day${day}_test")
     check(part1(testInput) == 31)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 29)
 
     val input = readInput("Day${day}")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
