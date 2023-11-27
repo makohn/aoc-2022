2734d9ea429b0099b32c8a4ce3343599b522b321
Marek Kohn <me@makohn.de>
2023-11-27 21:26:23 +0100

    Add solution for day 16 part 1

--- src/Day16.kt
+++ src/Day16.kt
@@ -4,83 +4,59 @@ fun main() {
 
     val day = "16"
 
-    data class Valve(val idx: Int, val name: String, val flowRate: Int, val tunnels: List<String>)
+    data class Valve(val idx: Int, val flowRate: Int, val tunnels: List<String>)
 
     fun parseInput(input: List<String>) = input
         .map { Regex("([A-Z]{2}|\\d+)").findAll(it).map { res -> res.value }.toList() }
-        .mapIndexed { idx, res -> Valve(idx, res[0], res[1].toInt(), res.drop(2)) }
+        .mapIndexed { idx, res -> res[0] to Valve(idx, res[1].toInt(), res.drop(2)) }
+        .toMap()
 
     fun part1(input: List<String>): Int {
         val valves = parseInput(input)
         println(valves)
 
-        val valveMap = valves.associateBy { it.name }
+        data class Configuration(val openValvesMask: Long, val atValve: String)
+        val maxTime = 30
+        val dp = Array(maxTime + 1) { HashMap<Configuration, Int>() }
+        fun memoize(time: Int, openValvesMask: Long, atValve: String, pressure: Int) {
+            val config = Configuration(openValvesMask, atValve)
+            // Get the current pressure for this valve
+            val current = dp[time][config]
+            // Set the time-configuration pressure to the given pressure if it doesn't exit or is lower
+            if (current == null || pressure > current) dp[time][config] = pressure
+        }
+        // In the beginning we are at valve AA, no valve open, no pressure released
+        memoize(0, 0, "AA", 0)
 
-        // Floyd-Warshall could be used to determine the shortest distance between all pairs of valves
-        // But it isn't the most efficient algorithm here, as it calculates the shortest distance between _all_ pairs
-        // We are only looking at a select number of nodes/valves
-
-        // BFS
-        val distanceMap = HashMap<Valve, HashMap<Valve, Int>>()
-        for (valve in valves) {
-
-            if (valve.name != "AA" && valve.flowRate == 0) continue
-
-            distanceMap.computeIfAbsent(valve) { hashMapOf(valve to 0, valveMap["AA"]!! to 0) }
-
-            val visited = hashSetOf(valve.name)
-            val queue = ArrayDeque<Pair<Int, Valve>>()
-            queue += 0 to valve
-
-            while (queue.isNotEmpty()) {
-                val (distance, position) = queue.removeFirst()
-                for (neighbor in position.tunnels) {
-                    if (neighbor in visited) continue
-                    visited.add(neighbor)
-                    val neighborValve = valveMap[neighbor]!!
-                    if (neighborValve.flowRate != 0) {
-                        distanceMap[valve]!![neighborValve] = distance + 1
-                    }
-                    queue += distance + 1 to neighborValve
+        for (time in 0 ..< maxTime) {
+            // Get all memoized configurations and their pressure level at the current time
+            for ((config, pressure) in dp[time]) {
+                val (openValvesMask, valveName) = config
+                val valve = valves[valveName]!!
+                val valvePos = 1L shl valve.idx
+                // if this configuration's valve's flow rate is relevant and the valve is not yet open...
+                if (valve.flowRate != 0 && (valvePos and openValvesMask) == 0L) {
+                    // ...open it and increase pressure level correspondingly
+                    memoize(
+                        /*time*/ time + 1,
+                        /*openValveMask*/ valvePos or openValvesMask,
+                        /*atValve*/ valveName,
+                        /*pressure*/ pressure + (maxTime - time -1) * valve.flowRate
+                    )
+                }
+                // Now go to all the neighbouring valves (without opening them yet)
+                for (neighbourValve in valve.tunnels) {
+                    memoize(
+                        /*time*/ time + 1,
+                        /*openValveMask*/ openValvesMask,
+                        /*atValve*/ neighbourValve,
+                        /*pressure*/ pressure
+                    )
                 }
             }
-            distanceMap[valve]!!.remove(valve)
-            if (valve.name != "AA") {
-                distanceMap[valve]?.remove(valveMap["AA"])
-            }
         }
 
-        println()
-        distanceMap.keys.forEach {
-            println("${it.name}: ${distanceMap[it]?.map { (k, v) -> k.name to v }}")
-        }
-
-        // DFS with memoization:
-        data class State(val time: Int, val valve: Valve, val openValveBitMask: Long)
-        val cache = HashMap<State, Int>()
-
-        fun dfs(state: State): Int {
-            println(state)
-            if (state in cache) return cache[state]!!
-
-            val (time, valve, bitmask) = state
-            var maxVal = 0
-            for (neighbor in distanceMap[valve]!!.keys) {
-                val bit = 1L shl valve.idx
-                if (bitmask and bit != 0L) continue
-                val remainingTime = time - distanceMap[valve]!![neighbor]!! - 1
-                if (remainingTime <= 0) continue
-                maxVal = max(maxVal,
-                    dfs(State(remainingTime, neighbor, bitmask or bit))
-                            + neighbor.flowRate * remainingTime)
-            }
-            cache[State(time, valve, bitmask)] = maxVal
-            return maxVal
-        }
-
-        val ans = dfs(State(30, valveMap["AA"]!!, 0))
-        println(ans)
-        return 0
+        return dp[maxTime].maxOf { (_, pressure) -> pressure }
     }
 
     fun part2(input: List<String>): Int {
